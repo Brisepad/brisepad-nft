61a542924b11dd4fca4e6f6396fe1de51db4df60
OkekeChristian24 <christian.okeke24@gmail.com>
2022-09-30 08:15:54 -0700

    chore: clean up

--- src/index.jsx
+++ src/index.jsx
@@ -4,16 +4,11 @@ import { Web3ReactProvider } from '@web3-react/core'
 import App from './App';
 import './index.css';
 import { getLibrary } from './utils/web3React';
-// import MetamaskProvider from './components/metamaskprovider/MetaMaskProvider';
 
 
 ReactDOM.render(
     <Web3ReactProvider getLibrary={getLibrary}>
-        {/* <MetamaskProvider>
-            <App />
-        </MetamaskProvider> */}
         <App />
-
     </Web3ReactProvider>,
     document.getElementById('root')
 );
