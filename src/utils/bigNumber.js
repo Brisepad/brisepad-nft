d795d8464ea118cd4085d214f75c9028f024dc0e
OkekeChristian24 <christian.okeke24@gmail.com>
2022-09-30 07:22:25 -0700

    first commit

---
+++ src/utils/bigNumber.js
@@ -0,0 +1,11 @@
+import BigNumber from 'bignumber.js'
+
+BigNumber.config({
+    EXPONENTIAL_AT: 1000,
+    DECIMAL_PLACES: 80,
+})
+
+export const BIG_ZERO = new BigNumber(0)
+export const BIG_ONE = new BigNumber(1)
+export const BIG_NINE = new BigNumber(9)
+export const BIG_TEN = new BigNumber(10)
